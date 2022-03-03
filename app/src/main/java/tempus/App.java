b6485ea1695a2c6383572f97bc7a2c98f1828832
diwangs <diwangs@cs.jhu.edu>
2022-03-03 14:27:56 -0500

    Started migrating template

--- app/src/main/java/tempus/App.java
+++ app/src/main/java/tempus/App.java
@@ -9,7 +9,6 @@ import java.net.URL;
 import com.uppaal.model.core2.Document;
 import com.uppaal.model.core2.PrototypeDocument;
 import com.uppaal.model.system.*;
-import com.uppaal.model.system.symbolic.*;
 import com.uppaal.engine.Engine;
 import com.uppaal.engine.Problem;
 import com.uppaal.engine.QueryFeedback;
@@ -30,6 +29,31 @@ public class App {
 
             // Model contruction (reading XML for now)
             Document doc = new PrototypeDocument().load(new URL("file:///home/diwangs/Codes/PhD/tempus/models/packet-based.xml"));
+			Link.createSampleLink(doc);
+
+			// System declaration
+			doc.setProperty("system", 
+				"L_1_20 = Link(10, 1, 20, 99, 1);" +
+				"L_2_20 = Link(11, 2, 20, 99, 1);" +
+				"L_3_21 = Link(12, 3, 21, 99, 1);" +
+				"L_4_21 = Link(13, 4, 21, 99, 1);" +
+				"L_20_21 = Link(14, 21, 20, 99, 1);" +
+				
+				"H1 = PingHost(1);" +
+				"H2 = PongHost(4);" + // Ack
+				
+				"S20 = Switch(20);" +
+				"S21 = Switch(21);" +
+				
+				"P1 = Packet(1, 1, 4);" + // Ping
+				"P2 = Packet(2, 4, 1);" + // Pong
+				
+				"system H1, H2, P1, P2, S20, S21, L_1_20, L_2_20, L_3_21, L_4_21, L_20_21;"
+			);
+
+			doc.save("test.xml");
+			
+			// System.out.println(doc.getTemplate("Link").getProperties());
             
             // Engine
             Engine engine = new Engine();
@@ -41,7 +65,7 @@ public class App {
             UppaalSystem sys = engine.getSystem(doc, problems);
 
             // Statistical model-checking:
-			Query smcq = new Query("Pr[# <= 40; 10000](<> P1.on_receiver)", "what is the probability of finishing?");
+			Query smcq = new Query("Pr[# <= 40](<> P1.on_receiver)", "what is the probability of finishing?");
             System.out.println("===== SMC check: " + smcq.getFormula() + " =====");
 			QueryResult res = engine.query(sys, options, smcq, qf);
 			System.out.println("Result: " + res);
---
+++ app/src/main/java/tempus/Link.java
@@ -0,0 +1,127 @@
+package tempus;
+
+import com.uppaal.model.core2.Document;
+import com.uppaal.model.core2.Template;
+import com.uppaal.model.core2.Edge;
+import com.uppaal.model.core2.Location;
+import com.uppaal.model.core2.Property;
+
+public class Link {
+
+    public enum LKind {
+        name, init, urgent, committed, invariant, exponentialrate, comments
+    };
+
+    public enum EKind {
+        select, guard, synchronisation, assignment, comments
+    };
+
+    public static void setLabel(Location l, LKind kind, Object value, int x, int y) {
+        l.setProperty(kind.name(), value);
+        Property p = l.getProperty(kind.name());
+        p.setProperty("x", x);
+        p.setProperty("y", y);
+    }
+
+    public static Location addLocation(Template t, String name, String exprate,
+									   int x, int y)
+    {
+        Location l = t.createLocation();
+        t.insert(l, null);
+        l.setProperty("x", x);
+        l.setProperty("y", y);
+		if (name != null)
+			setLabel(l, LKind.name, name, x, y-28);
+		if (exprate != null)
+			setLabel(l, LKind.exponentialrate, exprate, x, y-28-12);
+        return l;
+    }
+
+    public static void setLabel(Edge e, EKind kind, String value, int x, int y) {
+        e.setProperty(kind.name(), value);
+        Property p = e.getProperty(kind.name());
+        p.setProperty("x", x);
+        p.setProperty("y", y);
+    }
+
+    public static Edge addEdge(Template t, Location source, Location target,
+							   String guard, String sync, String update)
+    {
+        Edge e = t.createEdge();
+        t.insert(e, null);
+        e.setSource(source);
+        e.setTarget(target);
+        int x = (source.getX()+target.getX())/2;
+        int y = (source.getY()+target.getY())/2;
+        if (guard != null) {
+            setLabel(e, EKind.guard, guard, x-15, y-28);
+        }
+        if (sync != null) {
+            setLabel(e, EKind.synchronisation, sync, x-15, y-14);
+        }
+        if (update != null) {
+            setLabel(e, EKind.assignment, update, x-15, y);
+        }
+        return e;
+    }
+
+    public static Template createSampleLink(Document doc) {
+        System.out.println("Creating links");
+        Template t = doc.createTemplate(); 
+		t.setProperty("name", "Experiment");
+		t.setProperty("declaration", "int v;\n\nclock x,y,z;");
+        System.out.println(t.getProperties());
+
+		// the template has initial location:
+		Location l0 = addLocation(t, "L0", "1", 0, 0);
+		l0.setProperty("init", true);
+		// add another location to the right:
+		Location l1 = addLocation(t, "L1", null, 150, 0);
+		setLabel(l1, LKind.invariant, "x<=10", l1.getX()-7, l1.getY()+10);
+		// add another location below to the right:
+		Location l2 = addLocation(t, "L2", null, 150, 150);
+		setLabel(l2, LKind.invariant, "y<=20", l2.getX()-7, l2.getY()+10);
+		// add another location below:
+		Location l3 = addLocation(t, "L3", "1", 0, 150);
+		// add another location below:
+		Location lf = addLocation(t, "Final", null, -150, 150);
+		// create an edge L0->L1 with an update
+		Edge e = addEdge(t, l0, l1, "v<2", null, "v=1,\nx=0");
+		e.setProperty(EKind.comments.name(), "Execute L0->L1 with v=1");
+
+		// create some more edges:
+		addEdge(t, l1, l2, "x>=5", null, "v=2,\ny=0");
+		addEdge(t, l2, l3, "y>=10", null, "v=3,\nz=0");
+		addEdge(t, l3, l0, null, null, "v=4");
+		addEdge(t, l3, lf, null, null, "v=5");
+
+        doc.insert(t, null);
+        System.out.println("Link created");
+
+        return t;
+    }
+
+    public static Template createSampleLink2(Document doc) {
+        System.out.println("Creating links");
+        Template t = doc.createTemplate(); 
+		t.setProperty("name", "LinkJava");
+        t.setProperty("parameter", "const ent_id_t eid, ent_id_t endpoint1, ent_id_t endpoint2, int success_odds, int fail_odds");
+		t.setProperty("declaration", 
+            "clock t;" +
+            "pkt_id_t temp_pid;" +
+            "ent_id_t temp_src_eid;"
+        );
+
+        // First location
+        Location l0 = addLocation(t, "idle", null, 0, 0);
+        l0.setProperty("init", true);
+
+        // Location l1 = addLocation(t, "on_link", null, 10, 0);
+        // l1.setProperty(arg0, arg1)
+
+        doc.insert(t, null);
+        System.out.println("Link created");
+
+        return t;
+    }
+}
